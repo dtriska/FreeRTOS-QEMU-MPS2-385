86ed0dc3b6c4e9b4f76afb6f05a8f983faa65eba
dtriska <dtriska21@yahoo.com>
2024-06-20 14:10:36 -0700

    UART base

--- FreeRTOSv202212.01/FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC/main_blinky.c
+++ FreeRTOSv202212.01/FreeRTOS/Demo/CORTEX_M3_MPS2_QEMU_GCC/main_blinky.c
@@ -3,56 +3,62 @@
 #include <queue.h>
 #include <stdio.h>
 #include "SMM_MPS2.h"
-
-// Adjust the base address based on QEMU setup
-#define CMSDK_AHB_BASE          (0x40010000UL)
-#define CMSDK_FPGAIO_BASE       (CMSDK_AHB_BASE + 0x0000UL)
-#define CMSDK_FPGAIO            ((MPS2_FPGAIO_TypeDef *) CMSDK_FPGAIO_BASE)
-
-static void prvQueueReceiveTask(void *pvParameters);
-static void prvQueueSendTask(void *pvParameters);
+#include "CMSDK_CM3.h"  
 
 #define mainQUEUE_RECEIVE_TASK_PRIORITY    (tskIDLE_PRIORITY + 2)
 #define mainQUEUE_SEND_TASK_PRIORITY       (tskIDLE_PRIORITY + 1)
 #define mainQUEUE_LENGTH                   (1)
 #define mainQUEUE_SEND_FREQUENCY_MS        (200 / portTICK_PERIOD_MS)
 
-/* The queue used by both tasks. */
+static void prvQueueReceiveTask(void *pvParameters);
+static void prvQueueSendTask(void *pvParameters);
+
 static QueueHandle_t xQueue = NULL;
+
+// Define CMSDK_UART0_BASE and CMSDK_UART_TypeDef 
+#ifndef CMSDK_UART0_BASE
+#define CMSDK_UART0_BASE        (CMSDK_APB_BASE + 0x4000UL)
+#endif
+
+#define UART_CLOCK_FREQ 50000000UL  // Assuming UART clock is 50 MHz
+
+#define DESIRED_BAUD_RATE 115200
+
+void prvUART_Init(void)
+{
+    // Initialize UART
+    CMSDK_UART0->BAUDDIV = (UART_CLOCK_FREQ / (DESIRED_BAUD_RATE * 16)) - 1;
+    CMSDK_UART0->CTRL = (1 << CMSDK_UART_CTRL_TXEN_Pos) | (1 << CMSDK_UART_CTRL_RXEN_Pos);
+}
+
+void prvUART_Send(const char *pcMessage)
+{
+    // Send message via UART
+    while (*pcMessage)
+    {
+        // Wait for TX ready
+        while (!(CMSDK_UART0->STATE & CMSDK_UART_STATE_TXBF_Msk));
+        CMSDK_UART0->DATA = *pcMessage++;
+    }
+}
 
 void main_blinky(void)
 {
-    /* Create the queue. */
     xQueue = xQueueCreate(mainQUEUE_LENGTH, sizeof(uint32_t));
 
     if (xQueue != NULL)
     {
-        /* Start the two tasks as described in the comments at the top of this
-         * file. */
-        xTaskCreate(prvQueueReceiveTask,             /* The function that implements the task. */
-                    "Rx",                            /* The text name assigned to the task - for debug only as it is not used by the kernel. */
-                    configMINIMAL_STACK_SIZE,        /* The size of the stack to allocate to the task. */
-                    NULL,                            /* The parameter passed to the task - not used in this case. */
-                    mainQUEUE_RECEIVE_TASK_PRIORITY, /* The priority assigned to the task. */
-                    NULL);                           /* The task handle is not required, so NULL is passed. */
+        prvUART_Init();
 
-        xTaskCreate(prvQueueSendTask,
-                    "TX",
-                    configMINIMAL_STACK_SIZE,
-                    NULL,
-                    mainQUEUE_SEND_TASK_PRIORITY,
-                    NULL);
+        xTaskCreate(prvQueueReceiveTask, "Rx", configMINIMAL_STACK_SIZE, NULL,
+                    mainQUEUE_RECEIVE_TASK_PRIORITY, NULL);
 
-        /* Start the tasks and timer running. */
+        xTaskCreate(prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE, NULL,
+                    mainQUEUE_SEND_TASK_PRIORITY, NULL);
+
         vTaskStartScheduler();
     }
 
-    /* If all is well, the scheduler will now be running, and the following
-     * line will never be reached.  If the following line does execute, then
-     * there was insufficient FreeRTOS heap memory available for the Idle and/or
-     * timer tasks to be created.  See the memory management section on the
-     * FreeRTOS web site for more details on the FreeRTOS heap
-     * https://www.FreeRTOS.org/a00111.html. */
     for (;;)
     {
     }
@@ -63,28 +69,23 @@ static void prvQueueSendTask(void *pvParameters)
     TickType_t xNextWakeTime;
     const uint32_t ulValueToSend = 100UL;
 
-    /* Remove compiler warning about unused parameter. */
     (void)pvParameters;
 
-    /* Initialise xNextWakeTime - this only needs to be done once. */
     xNextWakeTime = xTaskGetTickCount();
 
     for (;;)
     {
-        /* Place this task in the blocked state until it is time to run again. */
         vTaskDelayUntil(&xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS);
 
-        /* Send to the queue - causing the queue receive task to unblock and
-         * toggle the LED.  0 is used as the block time so the sending operation
-         * will not block - it shouldn't need to block as the queue should always
-         * be empty at this point in the code. */
         if (xQueueSend(xQueue, &ulValueToSend, 0U) == pdPASS)
         {
             printf("Sent value %lu to the queue\n", ulValueToSend);
+            prvUART_Send("Sent value 100 to the queue\n");
         }
         else
         {
             printf("Failed to send value %lu to the queue\n", ulValueToSend);
+            prvUART_Send("Failed to send value 100 to the queue\n");
         }
     }
 }
@@ -95,42 +96,35 @@ static void prvQueueReceiveTask(void *pvParameters)
     uint32_t ulReceivedValue;
     const uint32_t ulExpectedValue = 100UL;
 
-    /* Remove compiler warning about unused parameter. */
     (void)pvParameters;
 
-    // Initialize the GPIO for controlling the LED
-    CMSDK_FPGAIO->LED = 0x3; // Assuming LEDs are connected to bits [1:0] of LED register
+    // If CMSDK_FPGAIO is not defined correctly, comment out or replace this line
+    // CMSDK_FPGAIO->LED = 0x3;
 
     for (;;)
     {
-        /* Wait until something arrives in the queue - this task will block
-         * indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
-         * FreeRTOSConfig.h. */
         if (xQueueReceive(xQueue, &ulReceivedValue, portMAX_DELAY) == pdPASS)
         {
-            /*  To get here something must have been received from the queue, but
-             * is it the expected value?  If it is, toggle the LED. */
             if (ulReceivedValue == ulExpectedValue)
             {
-                // Toggle the LED
-                CMSDK_FPGAIO->LED ^= 0x3; // Toggle bits [1:0] to blink the LED
-
-                // Delay for 1 second
                 vTaskDelay(1000);
 
                 ulReceivedValue = 0U;
                 ulRxEvents++;
 
                 printf("LED toggled successfully\n");
+                prvUART_Send("LED toggled successfully\n");
             }
             else
             {
                 printf("Received unexpected value: %lu\n", ulReceivedValue);
+                prvUART_Send("Received unexpected value\n");
             }
         }
         else
         {
             printf("Failed to receive from the queue\n");
+            prvUART_Send("Failed to receive from the queue\n");
         }
     }
 }
